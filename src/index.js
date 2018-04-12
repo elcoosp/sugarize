d279def936c2f2f8a2626197829829e9e4d74c32
Elcoosp <elcoosp@gmail.com>
2018-04-12 03:55:00 +0200

    fix: wrong path in require

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  sugar: require('../sugar'),
-  sugarSlow: require('../sugarSlow'),
-  sugarize: require('../sugarize'),
-  sugarizeSlow: require('../sugarizeSlow')
+  sugar: require('./sugar'),
+  sugarSlow: require('./sugarSlow'),
+  sugarize: require('./sugarize'),
+  sugarizeSlow: require('./sugarizeSlow')
 }
